ed8e70a7e278321576ed30fb5c4981d1d6481feb
Carlos Fernandes da Cunha Junior <carlos.junior@vcpericia.intranet>
2023-05-03 11:13:14 -0400

    nova divisão de classes

--- temp.py
+++ temp.py
@@ -5,10 +5,12 @@ import ssl
 import pandas as pd
 import simplekml
 
-class Coletor:
-    def __init__(self, city):
-        self.city=self.limpa_input(city)
+class VerificadorLocal:
+    def __init__(self):
+        self.input_cidade()
         self.lista_bairros_cidades()
+        self.verifica_cidade()
+        self.verifica_bairro()
 
     def lista_bairros_cidades(self):
         with open('cidades.txt', 'r') as arquivo:
@@ -28,7 +30,7 @@ class Coletor:
     def verifica_cidade(self):
         while True:
             if self.city in self.cidades: 
-                print('Cidade encontrada! Por favor aguarde enquanto coleto os elementos para você\n')
+                print('Cidade encontrada!\n')
                 break
             else: 
                 print(u'Cidade não encontrada!\nPor favor, verifique o nome da cidade e tente novamente.\n')
@@ -36,19 +38,32 @@ class Coletor:
         self.verificador = self.city == 'campo grande'
 
     def verifica_bairro(self):
-        print(u'Antes de prosseguir, verifique como está o nome do bairro no site do Infoimóveis :)\n')
         if self.verificador:
+            print(u'Antes de prosseguir, verifique como está o nome do bairro no site do Infoimóveis :)\n')
             while True:
                 self.input_bairro()
                 if self.bairro in self.bairros: 
-                    print('Bairro encontrado! Por favor aguarde enquanto coleto os elementos para você\n')
+                    print('Bairro encontrado!\n')
                     break
                 else: print(u'Bairro não encontrado!\nPor favor, verifique o nome do bairro e tente novamente.\n')
 
-    def adequa_p_link(value):
-        aux = value.split()
-        value_link=''
-        for i in aux:
-            value_link += i + '-'
-        value_link = value_link.rstrip('-')
-        return value_link
+    def get_local(self):
+        if self.verificador: return self.bairro
+        else: return self.city
+ 
+
+class PegaURL():
+    def __init__(self, local):
+        local_link = self.adequa_p_link(local)
+
+    def adequa_p_link(self, value):
+            aux = value.split()
+            value_link=''
+            for i in aux:
+                value_link += i + '-'
+            value_link = value_link.rstrip('-')
+            return value_link
+
+
+local = VerificadorLocal().get_local()
+PegaURL(local)
